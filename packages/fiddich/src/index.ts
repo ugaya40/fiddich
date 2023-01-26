0d970d3eddd2a770f1f033a1411e64e62a3fdf35
ugaya40(Masanori Onoue) <ugaya40@hotmail.com>
2023-01-26 13:52:09 +0900

    add SetterOrUpdater export.
    v0.1.15

--- packages/fiddich/src/index.ts
+++ packages/fiddich/src/index.ts
@@ -6,3 +6,5 @@ export { SubFiddichRoot } from './components/SubFiddichRoot';
 export { useFiddichState } from './hooks/useFiddichState';
 export { useFiddichValue } from './hooks/useFiddichValue';
 export { useSetFiddichState } from './hooks/useSetFiddichState';
+
+export type { SetterOrUpdater } from './core';
